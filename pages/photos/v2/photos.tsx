73663bb628f40ea1414a510beadcf9dcbfff6826
mritun1 <m.mushahary1@gmail.com>
2023-04-06 13:15:02 +0530

    dd

--- pages/photos/v2/photos.tsx
+++ pages/photos/v2/photos.tsx
@@ -85,10 +85,13 @@ export default function Photos() {
                                 const { SL, ID, PHOTO_TITLE, PHOTO_CATEGORY_NAME, PHOTO_URL, PHOTO_DES } = post
                                 if (SL == gal1) {
                                     gal1 = gal1 + 3
-                                    return <><Link key={ID} href={"/photos/v2/" + ID + "/" + PHOTO_TITLE}>
+                                    return (<>
+                                    <Link key={ID} href={"/photos/v2/" + ID + "/" + PHOTO_TITLE}>
                                         <img
                                             src={PHOTO_URL} />
-                                    </Link><iframe src="https://drive.google.com/file/d/1Nynv4xQc4lOopI6aJnuzyhbogI3S5N7w/preview" width="640" height="480" allow="autoplay"></iframe></>
+                                    </Link>
+                                    <iframe src="https://drive.google.com/file/d/1Nynv4xQc4lOopI6aJnuzyhbogI3S5N7w/preview" width="100%" height="auto" allow="autoplay"></iframe>
+                                    </>)
                                     
                                 }
                                 
